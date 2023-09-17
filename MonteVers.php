8cfa29f302708a0704c309baceb61c68b86f0d53
hea07 <118740491+hea07@users.noreply.github.com>
2023-09-17 11:08:49 +0200

    Update MonteVers.php

--- MonteVers.php
+++ MonteVers.php
@@ -1,11 +1,11 @@
 <?php
-$distFileUrl = 'https://example.com/path/to/your/file.dist'; // Replace with your .dist file URL
+$distFileUrl = 'https://swscan.apple.com/content/catalogs/others/index-12-1.sucatalog'; // Replace with your .dist file URL
 
 $distContent = file_get_contents($distFileUrl);
 
 if ($distContent !== false) {
     $lastValue = null; // Initialize a variable to store the last found value
-    $pattern = '/key\s*:\s*Bambus\s*\n\s*string\s*:\s*(.*?)\s*\n/s';
+    $pattern = '/key\s*:\s*ExtendedMetaInfo\s*\n\s*string\s*:\s*(.*?)\s*\n/s';
 
     // Iterate through the matches and update $lastValue each time
     preg_match_all($pattern, $distContent, $matches);
