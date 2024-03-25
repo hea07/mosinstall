7407a9820ea2c7b7056029edfc77677759aa91bf
hea07 <118740491+hea07@users.noreply.github.com>
2024-03-25 15:59:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ read -r sonomaVersion sonomaLink <<<"$(curl -s https://latest-sonoma.hischem.de 
 sonomaOldVersion="14.1.2 (non M3)"
 sonomaOldVersionLink="https://swcdn.apple.com/content/downloads/24/37/052-09398-A_DIKZGBNOM0/y4rz9dued01dtyl65nxqgd08wj2ar5cr6v/InstallAssistant.pkg"
 
-sonomaOldVersionM3="14.1.2 (only M3)"
+sonomaOldVersionM3="14.1.2 (M3 only)"
 sonomaOldVersionM3Link="https://swcdn.apple.com/content/downloads/54/47/052-09460-A_HHL1JV64MF/b7arop3bkdru7i7anbw4qdlij5tqoz20hp/InstallAssistant.pkg"
 
 #
@@ -59,9 +59,9 @@ function printInfo() {
     #
 
     echo
-    echo -e "${RED}[INFO]: This script can remove all data on your computer!${NC}"
-    echo -e "${RED}[INFO]: Press CTRL-Z to cancel this process anytime!${NC}"
-    echo -e "${GREEN}[INFO]:${NC} Starting processes ..."
+    echo -e "${RED}This script can remove all data on your computer!${NC}"
+    echo -e "${RED}Press CTRL-Z to cancel this process anytime!${NC}"
+    echo -e "${GREEN}Starting processes ...${NC}"
 
 }
 
@@ -82,57 +82,57 @@ function eraseDisk() {
         diskName="Macintosh HD"
     fi
 
-    echo -e "${GREEN}[INFO]:${NC} Destination disk is: ${diskPath}"
+    echo -e "${GREEN}Destination disk is: ${diskPath}${NC}"
     echo
     echo -e "${RED}[CHOICE]: Do you want to delete all data on this computer? (y/n)${NC}"
 
     case $arg1 in
 
-    [YyNn])
-        answer="$arg2"
-        ;;
+        [YyNn])
+            answer="$arg1"
+            ;;
 
-    *)
-        echo -e "${GREEN}SKIPPING IN 5 SECONDS...${NC}"
-        read -t 5 answer </dev/tty || answer="n" # Timeout set to 5 seconds, defaults to "no"
-        echo $@
-        ;;
+        *)
+            echo -e "${GREEN}SKIPPING IN 5 SECONDS...${NC}"
+            read -t 5 answer </dev/tty || answer="n" # Timeout set to 5 seconds, defaults to "no"
+            echo $@
+            ;;
     esac
 
     if [ "$answer" != "${answer#[Yy]}" ]; then
 
-        echo -e "${GREEN}[INFO]:${NC} Ckecking Volumes ..."
+        echo -e "${GREEN}Ckecking Volumes ...${NC}"
         internalDisk=$(diskutil list | grep "synthesized" | awk -F " " {'print $1'} | awk -F "/" {'print $3'} | head -1)
-        echo -e "${GREEN}[INFO]:${NC} APFS synthesized disk is: ${internalDisk}"
+        echo -e "${GREEN}APFS synthesized disk is: ${internalDisk}${NC}"
         DSK_MACINTOSH_HD=$(diskutil list $internalDisk | grep -i "${diskName}" | grep -vi "data" | awk {'print $NF'})
-        echo -e "${GREEN}[INFO]:${NC} APFS Macintosh HD disk is: ${DSK_MACINTOSH_HD}"
+        echo -e "${GREEN}APFS Macintosh HD disk is: ${DSK_MACINTOSH_HD}${NC}"
         DSK_MACINTOSH_HD_DATA=$(diskutil list $internalDisk | grep -i "DATA" | awk {'print $NF'})
 
         if [ -z "$DSK_MACINTOSH_HD_DATA" ]; then
-            echo -e "${GREEN}[INFO]:${NC} APFS Data HD not found. Skipping."
+            echo -e "${GREEN}APFS Data HD not found. Skipping.${NC}"
         else
-            echo -e "${GREEN}[INFO]:${NC} APFS Data HD disk is: ${DSK_MACINTOSH_HD_DATA}"
+            echo -e "${GREEN}APFS Data HD disk is: ${DSK_MACINTOSH_HD_DATA}${NC}"
             umount -f /dev/${DSK_MACINTOSH_HD_DATA} /dev/null &>/dev/null
-            echo -e "${GREEN}[INFO]:${NC} Deleting Data partition ..."
+            echo -e "${GREEN}Deleting Data partition...${NC}"
             diskutil apfs deleteVolume ${DSK_MACINTOSH_HD_DATA} &>/dev/null
         fi
 
         # erase volume, create new one called "Macintosh HD"
-        echo -e "${GREEN}[INFO]:${NC} Deleting Macintosh HD ..."
+        echo -e "${GREEN} Deleting Macintosh HD...${NC}"
         diskutil apfs eraseVolume ${DSK_MACINTOSH_HD} -name "Macintosh HD" &>/dev/null
         diskPath='/Volumes/Macintosh HD'
         diskName='Macintosh HD'
         diskutil mount /dev/${DSK_MACINTOSH_HD} &>/dev/null
 
         if [[ $? -ne 0 ]]; then
-            echo -e "${RED}[ERROR]:${NC} Error mounting Macintosh HD."
-            echo -e "${RED}[ERROR]:${NC} Please delete the volume yourself."
-            echo -e "${RED}[ERROR]:${NC} Exiting."
+            echo -e "${RED}[ERROR]: Error mounting Macintosh HD.${NC}"
+            echo -e "${RED}[ERROR]: Please delete the volume yourself.${NC}"
+            echo -e "${RED}[ERROR]: Exiting.${NC}"
             exit
         fi
 
     else
-        echo -e "${GREEN}[INFO]:${NC} Skipping disk deletion."
+        echo -e "${GREEN}Skipping disk deletion.${NC}"
     fi
 
 }
@@ -145,13 +145,13 @@ function downloadInstaller() {
     # downloads the installer
     #
 
-    echo -e "${GREEN}[INFO]:${NC} Set up folder ..."
+    echo -e "${GREEN} Set up folder...${NC}"
     # rm -rf "${diskPath}"/private/tmp/* >/dev/null 2>&1 ### This would delete manually downloaded Installers ...
     mkdir -p "${diskPath}"/private/tmp
     cd "${diskPath}/private/tmp/"
 
     echo
-    echo -e "${GREEN}[CHOICE]:${NC} Choose your macOS Version"
+    echo -e "${GREEN}[CHOICE]: Choose your macOS Version${NC}"
     echo
     echo -e "\t1. macOS Sonoma\t\t${sonomaVersion}"
     echo -e "\t2. macOS Ventura\t${venturaVersion}"
@@ -174,18 +174,18 @@ function downloadInstaller() {
     esac
 
     case $answer in
-    "1")
+    "1" | "14")
         macOSName="Sonoma"
         macOSVersion=${sonomaVersion}
         macOSUrl=${sonomaLink}
         ;;
-    "2")
+    "2" | "13")
         macOSName="Ventura"
         macOSVersion=${venturaVersion}
         macOSUrl=${venturaLink}
         ;;
 
-    "3")
+    "3" | "12")
         macOSName="Monterey"
         macOSVersion=${montereyVersion}
         macOSUrl=${montereyLink}
@@ -204,43 +204,43 @@ function downloadInstaller() {
         ;;
 
     *)
-        echo -e "${RED}[INFO]:${NC} Invalid selection, exiting now ..."
+        echo -e "${RED}Invalid selection, exiting now...${NC}"
         exit
         ;;
     esac
 
-    echo -e "${GREEN}[INFO]:${NC} You chose macOS ${macOSName} ${macOSVersion}"
+    echo -e "${GREEN}You chose macOS ${macOSName} ${macOSVersion}${NC}"
 
     if [[ -f "InstallAssistant.pkg" ]]; then
-        echo -e "${GREEN}[INFO]:${NC} InstallAssistant.pkg found, use that one? (Y/n)"
+        echo -e "${GREEN}InstallAssistant.pkg found, use that one? (Y/n)${NC}"
         read useIA </dev/tty
 
         case $useIA in
         [Yy]*)
-            echo -e "${GREEN}[INFO]:${NC} Using predownloaded Installer."
+            echo -e "${GREEN}Using predownloaded Installer.${NC}"
             return
             ;;
 
         [Nn]*)
-            echo -e "${GREEN}[INFO]:${NC} Deleting ..."
+            echo -e "${GREEN}Deleting...${NC}"
             rm InstallAssistant.pkg >/dev/null 2>&1
             ;;
 
         *)
-            echo -e "${GREEN}[INFO]:${NC} Invalid input, exiting ..."
+            echo -e "${GREEN}Invalid input, exiting...${NC}"
             exit
             ;;
         esac
     fi
 
     echo
-    echo -e "${GREEN}[INFO]:${NC} Downloading files from Apple ..."
+    echo -e "${GREEN}Downloading files from Apple...${NC}"
     curl -L --progress-bar -f -o InstallAssistant.pkg ${macOSUrl}
 
     if [[ $? -eq 0 ]]; then
-        echo -e "${GREEN}[INFO]:${NC} Download finished."
+        echo -e "${GREEN}Download finished.${NC}"
     else
-        echo -e "${RED}[ERROR]:${NC} Download failed. Exiting now!"
+        echo -e "${RED}[ERROR]: Download failed. Exiting now!${NC}"
         exit
     fi
 
@@ -252,12 +252,13 @@ function expandAndSet() {
     # Moving files to set up the InstallAssistant
     #
 
-    echo -e "${GREEN}[INFO]:${NC} Expanding Installer ..."
+    echo -e "${GREEN}Expanding Installer...${NC}"
     pkgutil --expand-full InstallAssistant.pkg Source
-    echo -e "${GREEN}[INFO]:${NC} Copying in place ..."
+
+    echo -e "${GREEN}Copying in place...${NC}"
     cp -R Source/Payload/Applications/"Install macOS ${macOSName}.app" "${diskPath}"/private/tmp/ &>/dev/null
 
-    echo -e "${GREEN}[INFO]:${NC} Changing permissions ..."
+    echo -e "${GREEN}Changing permissions...${NC}"
     SSPATH="Install macOS ${macOSName}.app/Contents/SharedSupport"
     mkdir -p "$SSPATH"
     /bin/chmod 0755 "$SSPATH"
@@ -266,10 +267,10 @@ function expandAndSet() {
     /usr/sbin/chown -R root:wheel "$SSPATH"/SharedSupport.dmg
     /usr/bin/chflags -h norestricted "$SSPATH"/SharedSupport.dmg
 
-    echo -e "${GREEN}[INFO]:${NC} Cleanup ..."
+    echo -e "${GREEN}Cleanup...${NC}"
     rm -rf Source
     rm -rf InstallAssistant.pkg
-    echo -e "${GREEN}[INFO]:${NC} Prerequisites done."
+    echo -e "${GREEN}Prerequisites done.${NC}"
 
 }
 
@@ -279,7 +280,7 @@ function unmountExternalDisks() {
     # will check for external media and will unmount them before installation
     #
 
-    echo -e "${GREEN}[INFO]:${NC} Looking for external drives and unmounting ..."
+    echo -e "${GREEN}Looking for external drives and unmounting ...${NC}"
     externalDisk=$(diskutil list | grep external | awk -F " " {'print $1'})
     diskutil umountDisk $externalDisk 2>/dev/null
 
@@ -300,7 +301,7 @@ function main() {
     expandAndSet
     unmountExternalDisks
 
-    echo -e "${GREEN}[INFO]:${NC} Starting installer ..."
+    echo -e "${GREEN} Starting installer...${NC}"
     "${diskPath}/private/tmp/Install macOS ${macOSName}.app"/Contents/MacOS/InstallAssistant_springboard >/dev/null
 
 }
